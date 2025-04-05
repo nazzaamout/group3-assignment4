1dd6e722ab72d690bedcb369ab0c92c40f348fcc
zealmehta15 <zealmehta15@gmail.com>
2025-04-05 16:45:24 -0600

    Your commit message describing changes

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,4 +1,6 @@
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
 const authenticate = (req, res, next) => {
   const authHeader = req.headers.authorization;
@@ -11,7 +13,7 @@ const authenticate = (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; // attach user info to the request
+    req.user = decoded; // attach user info to request
     next();
   } catch (err) {
     res.status(401).json({ error: "Invalid token" });
